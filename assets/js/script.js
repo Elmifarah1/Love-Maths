0f77b6a9ea1d5a3625dd768c9e0732c6ba1f624d
Elmi Farah <elmifarah98@gmail.com>
2024-05-16 13:57:22 +0000

    Created functions to run the game

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractAnswer() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivideQuestion() {
+
+}
